a3d4d0ab96eaafb4efb9449d016afedd10e4981e
Nhiyhe Adex <nhiyhe@gmail.com>
2016-10-05 16:51:18 +0100

    partial add comment to gigs added

--- controllers/gigController.js
+++ controllers/gigController.js
@@ -1,8 +1,11 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 var Gig = require("../models/gig");
+var Comment = require('../models/Comment');
 
 module.exports = function(app){
+
+    
     app.get('/gigs',function(req,res){
         Gig.find({}, function(err, gigs){
             if(err){
@@ -24,9 +27,35 @@ module.exports = function(app){
             }
         })
     })
+   
+   app.get('/gigs/:id/comment/new', function(req,res){
+       Gig.findById(req.params.id, function(err, foundGig){
+           if(err){
+               console.log(err);
+           }else{
+                res.render('gig/comment',{gig:foundGig});
+           }
+       })
+        
+   });
 
-
-
+   app.post('/gigs/:id/comment', function(req,res){
+       Gig.findById(req.params.id, function(err, foundGig){
+            if(err){
+                console.log(err);
+            }else{
+                Comment.create(req.body.comment, function(err, comment){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        foundGig.comments.push(comment);
+                        foundGig.save();
+                        res.redirect('/gigs');
+                    }
+                })
+            }
+       })
+   });
 
 
 
